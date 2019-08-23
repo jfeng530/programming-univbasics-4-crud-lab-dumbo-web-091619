689cb3ecec472aa3357d16810a73edcc5ecae289
Jacky Feng <jackyfeng530@yahoo.com>
2019-08-23 00:50:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "element"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number] = "element"
+  array[index_number] = element
 end
